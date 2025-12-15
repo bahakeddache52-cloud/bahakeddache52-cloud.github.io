632c1bc389aac14174e4a593dbde255d274875fa
baha keddache <bahakeddache52@gmail.com>
2025-12-15 19:12:39 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,4 @@
-// JavaScript used to set randomness for particles.
-// Could be done via SSR
+// هذا الكود بسيط جداً ويمكن توسيعه لاحقاً
+console.log("Welcome to Ronald's Portfolio site!");
 
-const RANDOM = (min, max) => Math.floor(Math.random() * (max - min + 1) + min)
-const PARTICLES = document.querySelectorAll('.particle')
-PARTICLES.forEach(P => {
-	P.setAttribute('style', `
-		--x: ${RANDOM(20, 80)};
-		--y: ${RANDOM(20, 80)};
-		--duration: ${RANDOM(6, 20)};
-		--delay: ${RANDOM(1, 10)};
-		--alpha: ${RANDOM(40, 90) / 100};
-		--origin-x: ${Math.random() > 0.5 ? RANDOM(300, 800) * -1 : RANDOM(300, 800)}%;
-		--origin-y: ${Math.random() > 0.5 ? RANDOM(300, 800) * -1 : RANDOM(300, 800)}%;
-		--size: ${RANDOM(40, 90) / 100};
-	`)
-})
+// يمكن إضافة تأثيرات حركية هنا لاحقاً باستخدام JavaScript
